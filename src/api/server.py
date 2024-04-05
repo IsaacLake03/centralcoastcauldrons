c528ba9df234f2fcc52ed814b2d654ded4fbfb85
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-04-05 11:27:19 -0700

    hmm trying to run locally

--- src/api/server.py
+++ src/api/server.py
@@ -12,13 +12,13 @@ Central Coast Cauldrons is the premier ecommerce site for all your alchemical de
 """
 
 app = FastAPI(
-    title="Central Coast Cauldrons",
+    title="Lakeside Cauldrons",
     description=description,
     version="0.0.1",
     terms_of_service="http://example.com/terms/",
     contact={
-        "name": "Lucas Pierce",
-        "email": "lupierce@calpoly.edu",
+        "name": "Isaac Lake",
+        "email": "ilake@calpoly.edu",
     },
 )
 
